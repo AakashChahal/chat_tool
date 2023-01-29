f5d91d9b463a2eb072f533641b2f5536041f2510
Aakash Chahal <aakash.chahal62@gmail.com>
2023-01-29 14:51:35 +0000

    updated styles for the home page + responsive

--- boredom/src/Home.js
+++ boredom/src/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Box, Grid, Button } from "@mui/material";
+import { Typography, Box, Grid, Button, TextField } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
@@ -13,6 +13,25 @@ const theme = createTheme({
     },
 });
 
+const messages = [
+    {
+        text: "Hello!",
+        author: "User 1",
+    },
+    {
+        text: "Hi! How are you?",
+        author: "User 2",
+    },
+    {
+        text: "I'm good, thanks. How about you?",
+        author: "User 1",
+    },
+    {
+        text: "I'm doing well, thanks!",
+        author: "User 2",
+    },
+];
+
 function Home() {
     const [videoSrc, setVideoSrc] = useState(null);
 
@@ -24,7 +43,6 @@ function Home() {
                 setVideoSrc(stream);
                 video.srcObject = stream;
                 video.play();
-                console.log("Stream:", stream);
             })
             .catch((error) => {
                 console.error("Error:", error);
@@ -32,23 +50,34 @@ function Home() {
     }, []);
     return (
         <ThemeProvider theme={theme}>
-            <Grid container spacing={2}>
-                <Grid item>
+            <Grid
+                container
+                spacing={2}
+                sx={{
+                    height: "100vh",
+                    width: "100vw",
+                }}
+            >
+                <Grid item xs={12} sm={6}>
                     <Box
                         sx={{
-                            border: "1px solid black",
-                            borderRadius: "10px",
-                            width: "30vw",
-                            height: "40vh",
+                            width: "100%",
+                            // height: "40vh",
                             marginTop: "7vh",
-                            marginLeft: "2vw",
+                            marginLeft: "1vw",
                         }}
                     >
                         {videoSrc ? (
                             <video
-                                src={videoSrc}
-                                autoPlay={true}
-                                controls={true}
+                                style={{
+                                    width: "100%",
+                                    height: "100%",
+                                    borderRadius: "10px",
+                                    boxShadow:
+                                        "0 19px 51px 0 rgba(0,0,0,0.16), 0 14px 19px 0 rgba(0,0,0,0.07)",
+                                }}
+                                autoPlay
+                                playsInline
                             />
                         ) : (
                             <p>Loading video...</p>
@@ -58,10 +87,10 @@ function Home() {
                         sx={{
                             border: "1px solid black",
                             borderRadius: "10px",
-                            width: "30vw",
+                            width: "100%",
                             height: "40vh",
                             marginTop: "7vh",
-                            marginLeft: "2vw",
+                            marginLeft: "1vw",
                         }}
                     >
                         <Typography
@@ -75,47 +104,67 @@ function Home() {
                         </Typography>
                     </Box>
                 </Grid>
-                <Grid item>
+                <Grid item xs={12} sm={6}>
                     <Box
                         sx={{
                             border: "1px solid black",
                             borderRadius: "10px",
-                            width: "60vw",
-                            height: "80vh",
+                            width: "100%",
+                            height: "87vh",
                             marginTop: "7vh",
                             marginRight: "2vw",
+                            marginLeft: "1vw",
                             position: "relative",
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "space-between",
                         }}
                     >
-                        <Typography
-                            sx={{
-                                textAlign: "center",
-                                margin: "auto",
-                                marginTop: "35%",
-                            }}
-                        >
-                            Main Content
-                        </Typography>
                         <Box
                             sx={{
-                                position: "absolute",
-                                bottom: "2vh",
-                                left: "2vw",
+                                height: "80vh",
+                                overflow: "auto",
+                                padding: "1rem",
                             }}
                         >
-                            <Button variant="contained" color="primary">
-                                Left
-                            </Button>
+                            {messages.map((message) => (
+                                <Typography sx key={message.message}>
+                                    {message.author}: {message.text}
+                                </Typography>
+                            ))}
                         </Box>
                         <Box
                             sx={{
-                                position: "absolute",
-                                bottom: "2vh",
-                                right: "2vw",
+                                display: "flex",
+                                justifyContent: "space-between",
+                                alignItems: "center",
+                                padding: "1rem",
+                                gap: "0.5rem",
                             }}
                         >
-                            <Button variant="contained" color="secondary">
-                                Right
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                sx={{
+                                    height: "4em",
+                                }}
+                            >
+                                Next
+                            </Button>
+                            <TextField
+                                id="outlined-basic"
+                                label="Message"
+                                variant="outlined"
+                                fullWidth
+                            />
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                sx={{
+                                    height: "4em",
+                                }}
+                            >
+                                Send
                             </Button>
                         </Box>
                     </Box>
