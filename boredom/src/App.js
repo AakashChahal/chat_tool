32d4a8044b1dec744caa6efc41ba9ac614420e48
Aakash Chahal <aakash.chahal62@gmail.com>
2023-01-30 14:50:55 +0000

    google signup

--- boredom/src/App.js
+++ boredom/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./Users/SignIn";
 import SignUp from "./Users/SignUp";
 import Home from "./Home";
@@ -10,17 +9,7 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <>
-                            <h1>Home</h1>
-                            <Link to="about">About</Link>
-                            <br />
-                            <img src={logo} className="App-logo" alt="logo" />
-                        </>
-                    }
-                />
+                <Route path="*" element={<SignUp />} />
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/home" element={<Home />} />
--- boredom/src/Users/SignUp.js
+++ boredom/src/Users/SignUp.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import {
+    Box,
+    Button,
+    FormControl,
+    FormHelperText,
+    Grid,
+    TextField,
+    Typography,
+} from "@mui/material";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+} from "firebase/auth";
+import { redirect } from "react-router-dom";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,20 +37,207 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+console.log(analytics);
+
+const theme = createTheme({
+    palette: {
+        red: {
+            main: "#ff0000",
+            secondary: "#881122",
+        },
+        green: {
+            main: "#00ff00",
+            secondary: "#118811",
+        },
+        blue: {
+            main: "#0000ff",
+            secondary: "#111188",
+        },
+    },
+});
 
 function SignUp() {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [passwordConfirm, setPasswordConfirm] = React.useState("");
+    const [passwordError, setPasswordError] = React.useState(false);
+    const [emailError, setEmailError] = React.useState(false);
+    const [emailHelperText, setEmailHelperText] = React.useState("");
+    const [passwordHelperText, setPasswordHelperText] = React.useState("");
+    const [passwordConfirmHelperText, setPasswordConfirmHelperText] =
+        React.useState("");
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handlePasswordConfirmChange = (event) => {
+        setPasswordConfirm(event.target.value);
+
+        if (event.target.value !== password) {
+            setPasswordError(true);
+            setPasswordConfirmHelperText("Passwords do not match");
+        } else {
+            setPasswordError(false);
+            setPasswordConfirmHelperText("");
+        }
+    };
+
+    const handleSignUp = () => {
+        if (email === "") {
+            setEmailError(true);
+            setEmailHelperText("Email cannot be empty");
+        } else {
+            setEmailError(false);
+            setEmailHelperText("");
+        }
+
+        if (password === "") {
+            setPasswordError(true);
+            setPasswordHelperText("Password cannot be empty");
+        } else {
+            setPasswordError(false);
+            setPasswordHelperText("");
+        }
+
+        if (passwordConfirm === "") {
+            setPasswordError(true);
+            setPasswordConfirmHelperText("Password cannot be empty");
+        } else {
+            setPasswordError(false);
+            setPasswordConfirmHelperText("");
+        }
+    };
+
+    const handleGoogleSignUp = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential =
+                    GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // ...
+                window.location.href = "/home";
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential =
+                    GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    };
+
     return (
-        <>
-            <Typography
-                component="h1"
-                variant="h1"
-                sx={{
-                    textAlign: "center",
-                }}
-            >
-                Sign Up
-            </Typography>
-        </>
+        <ThemeProvider theme={theme}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Typography
+                        component="h1"
+                        variant="h2"
+                        sx={{
+                            textAlign: "center",
+                        }}
+                    >
+                        Boredom
+                    </Typography>
+                </Grid>
+                <Grid
+                    item
+                    xs={12}
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        alignContent: "center",
+                    }}
+                >
+                    <Box
+                        sx={{
+                            width: "100%",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            gap: "1rem",
+                        }}
+                    >
+                        <Typography
+                            component="h2"
+                            variant="h4"
+                            sx={{
+                                color: "green.main",
+                                textAlign: "center",
+                            }}
+                        >
+                            Sign Up
+                        </Typography>
+                        <Box sx={{ width: "100%", textAlign: "center" }}>
+                            <FormControl
+                                sx={{
+                                    width: "50%",
+                                }}
+                            >
+                                <TextField
+                                    id="email"
+                                    label="Email Address"
+                                    aria-describedby="email-helper-text"
+                                />
+                                <FormHelperText id="email-helper-text">
+                                    We'll never share your email.
+                                </FormHelperText>
+                                <TextField
+                                    id="password"
+                                    type={"password"}
+                                    label="Password"
+                                />
+                            </FormControl>
+                        </Box>
+                        <Typography component={"h1"} variant={"h5"}>
+                            OR
+                        </Typography>
+                        {/* Google sign up using firebase */}
+                        <Button
+                            sx={{
+                                color: "blue.secondary",
+                                border: "1px solid blue",
+                                "&:hover": {
+                                    backgroundColor: "blue.main",
+                                    color: "white",
+                                },
+                                borderRadius: "0.5rem",
+                            }}
+                            onClick={handleGoogleSignUp}
+                        >
+                            Sign Up with Google{" "}
+                            <span
+                                style={{
+                                    margin: "auto 0.5rem auto 0.5rem",
+                                }}
+                            >
+                                <img
+                                    width={"20px"}
+                                    height={"20px"}
+                                    src="https://img.icons8.com/color/512/google-logo.png"
+                                    alt="google logo"
+                                />
+                            </span>
+                        </Button>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
     );
 }
 
